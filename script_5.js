78438a5676d9ebf61e4f23eac7ac09c8fcabd518
slvpdev <jonayco17@gmail.com>
2023-07-06 09:46:07 +0800

    update script5

--- script_5.js
+++ script_5.js
@@ -1,18 +1,56 @@
-function onChange(control, oldValue, newValue, isLoading, isTemplate) {
-    if (isLoading || newValue === '') {
-       return;
+// Additional Condition && current.getRecordClassName() !='u_task_child' && current.getRecordClassName() !='u_task_grandchild'
+var TaskChildCascadeDelete = Class.create();
+// Extend the global.AbstractAjaxProcessor class
+TaskChildCascadeDelete.prototype = Object.extendsObject(global.AbstractAjaxProcessor,{
+  deleteGrandChild: function() {
+
+    // delete grandchild records
+    var gr = new GlideRecord('u_task_grandchild');
+	var recordCount = 0;
+    gr.addQuery('u_parent_task_child', this.getParameter('sysparm_childID'));
+    gr.query();
+    while (gr.next()) {
+      gr.deleteRecord();
+		recordCount++;
     }
- 
-    var options = {
-     '0' : 'None',
-     '1001' : 'Active',
-     '1002' : 'Complete',
-     '1003' : 'Pending',
+
+
+    // delete child record
+    var child = new GlideRecord('u_task_child');
+    child.addQuery('sys_id', this.getParameter('sysparm_childID'));
+    child.query();
+    while (child.next()) {
+      child.deleteRecord();
     }
- 
-     var gprev = g_scratchpad.prevValue;
-     var prevValue = g_scratchpad.prevValue ? gprev : '0';
-         alert("User status changed from " + options[prevValue] + " to " + options[newValue]);
-     
-     g_scratchpad.prevValue = newValue;
- }
+
+	return recordCount;
+  },
+  type: 'TaskChildCascadeDelete'
+});
+
+// Client-side script Delete Child
+function taskChildCascadeDelete(){
+	var ga = new GlideAjax('global.TaskChildCascadeDelete');
+	ga.addParam('sysparm_name', 'deleteGrandChild');
+	ga.addParam('sysparm_childID', g_form.getUniqueValue());
+	ga.getXMLAnswer(function(response){
+		if(response > 0){
+				
+			alert("Deleted " + Math. trunc(response) + " Grandchildren. Record deleted.");
+		}else{
+			alert("Record deleted, no grandchild found.");
+		}
+		top.window.location = "https://dev176457.service-now.com/now/nav/ui/classic/params/target/u_task_child_list.do";
+	});
+}
+
+// Client-side script Delete Grandchild
+function taskChildCascadeDelete(){
+	var ga = new GlideAjax('global.TaskChildCascadeDelete');
+	ga.addParam('sysparm_name', 'deleteGrandChild');
+	ga.addParam('sysparm_childID', g_form.getUniqueValue());
+	ga.getXMLAnswer(function(response){
+			alert("Grandchild deleted");
+		top.window.location = "https://dev176457.service-now.com/now/nav/ui/classic/params/target/u_task_grandchild_list.do";
+	});
+}
